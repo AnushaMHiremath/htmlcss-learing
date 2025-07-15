fbe05d4f4ff99bba2682b2ad3c37d660dca24fe1
Anusha M Hiremath <anushamhiremath2003@gmail.com>
2025-07-15 22:13:45 +0530

    first commit

---
+++ Japp.js
@@ -0,0 +1,12 @@
+//dATE: 15/07/2025 Day:Tuesday to run  the command is node file_name.js 
+//console.log("hiiii");
+// let a ="Aai";// let block scoped eg for loop let means we can't REDECLARE(data_type variable_name = "value";) THE VALUES in let
+// console.log(typeof(a));// to check the type of data
+var a = "Anu";
+var a = "Anusha";// var is golbal scoped type we can REDECLARE AND REASSIGN THE VALUES
+console.log(a);
+
+
+
+
+
