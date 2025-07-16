cf1cfbc88ba8966aa3543fad4d578b6ae773fbc9
Anusha M Hiremath <anushamhiremath2003@gmail.com>
2025-07-16 11:17:49 +0530

    Update Japp.js with new code

--- Japp.js
+++ Japp.js
@@ -2,10 +2,14 @@
 //console.log("hiiii");
 // let a ="Aai";// let block scoped eg for loop let means we can't REDECLARE(data_type variable_name = "value";) THE VALUES in let
 // console.log(typeof(a));// to check the type of data
-var a = "Anu";
+/*var a = "Anu";
 var a = "Anusha";// var is golbal scoped type we can REDECLARE AND REASSIGN THE VALUES
 console.log(a);
+*/
+//Basic java Script Program 
 
+console.log("Hello World!!!")
+        
 
 
 
