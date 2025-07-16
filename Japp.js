d8eb0d70dca5a7119bf55f0dffd78b6f786d12a4
Anusha Mallikarjun Hiremath <152367847+AnushaMHiremath@users.noreply.github.com>
2025-07-16 17:09:25 +0530

    Update Japp.js

--- Japp.js
+++ Japp.js
@@ -1,14 +1,25 @@
-//dATE: 15/07/2025 Day:Tuesday to run  the command is node file_name.js 
-//console.log("hiiii");
-// let a ="Aai";// let block scoped eg for loop let means we can't REDECLARE(data_type variable_name = "value";) THE VALUES in let
-// console.log(typeof(a));// to check the type of data
-/*var a = "Anu";
+//Js first Bsic Program.
+console.log("hiiii");
+//=============================================================================================================================
+
+//Let
+ let a ="Aai";// let block scoped eg for loop let means we can't REDECLARE(data_type variable_name = "value";) THE VALUES in let
+console.log(typeof(a));// to check the type of data
+//=============================================================================================================================
+
+// Var 
+var a = "Anu";
 var a = "Anusha";// var is golbal scoped type we can REDECLARE AND REASSIGN THE VALUES
 console.log(a);
-*/
-//Basic java Script Program 
+//=============================================================================================================================
 
+//Basic java Script Program 
 console.log("Hello World!!!")
+//=============================================================================================================================
+
+
+
+
         
 
 
